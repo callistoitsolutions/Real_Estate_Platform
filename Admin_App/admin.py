4105d9dd17a631069848a5fe2f33012496b2b925
callistoitsolutions <callistoworkspace@gmail.com>
2026-03-06 16:04:47 +0530

    Update admin file 06-03-26 palash

--- Admin_App/admin.py
+++ Admin_App/admin.py
@@ -6,3 +6,8 @@ from Admin_App.models import *
 # Register your models here.
 
 admin.site.register(Admin_Login)
+
+
+######### Register Ameneties Details Model #####################
+
+admin.site.register(Ameneties_Details)
